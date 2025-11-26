f59a3808f6d546e20c7cad0fdc66765d733720bc
Ricardo Hernandez Alvarez <ricardohernandezalv656@gmail.com>
2025-11-26 02:34:05 -0600

    Arreglar url

--- Papeleria-Vista/src/store.js
+++ Papeleria-Vista/src/store.js
@@ -5,7 +5,7 @@ import axios from 'axios';
 // CONFIGURACIÓN INICIAL
 // ====================================================
 
-const API_URL = 'http://127.0.0.1:8000/api';
+const API_URL = import.meta.env.VITE_API_URL || 'http://127.0.0.1:8000/api';
 
 // ----------------------------------------------------
 // I. LÓGICA DE AUTENTICACIÓN (AUTH)
